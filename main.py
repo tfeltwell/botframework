8c957486529f7358a44b4d5b9d9156a614185a91
Tom Feltwell <tfeltwell@gmail.com>
2016-02-22 16:36:47 +0000

    Added an example bot

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def exmpTweet():
         print e
 
 ## 3) Search for something, do something with tweet ##
-def exmpSearch(noTweets):
+def exmpSearch():
     search_results = twitter.search(q='programming', count=30)
     try:
         for count, tweet in enumerate(search_results['statuses']):
@@ -78,7 +78,32 @@ def exmpFollowers():
 # Keep running until killed
 while(True):
 
-    exmpFollowers()
+    ## Example bot built by Tom, 
+    ## This will fetch tweets about Hastings (minus any celebs or other shit)
+    ## then send them the crack addiction rates :-)
+    ## Read this about building queries: https://dev.twitter.com/rest/public/search
+
+    # Get the PHE data from the database
+    query = (19, 94, 9, "2011/12") # Your query values need to be in here, in order of query (as below)
+    c.execute('SELECT count FROM Data WHERE indicator_id=? AND area_id=? AND parent_id=? AND date_info=?',query )
+    crackNumber = c.fetchone()[0]
+    myTweet = "Did you know that in 2011/12 there were "+str(crackNumber)+" people who used crack and or opiates in Hastings?"
+    print myTweet
+
+    # Do a Twitter search, iterate for each result
+    search_results = twitter.search(q='hastings -spencer -max -NE', count=10)
+    for i, result in enumerate(search_results["statuses"]):
+
+        # This bit prints stuff so we can see what it's doing
+        # print result.keys() # Debugging - This will show us all the data for each tweet
+        print i+1,':'
+        print 'This is their tweet:',result["text"]
+        print 'This is their name and ID:',result["user"]["name"],result["user"]["id_str"]
+        print 'This is the ID of their tweet',result["id"]
+
+        # Do the tweeting
+        replyID = result["id"] # Get the ID of the tweet so we can reply to it
+        # twitter.update_status(status=myTweet, in_reply_to_status_id=replyID)
 
     print "Sleeping for 5 minutes now"
     time.sleep(300) # Sleep for 5 minutes
